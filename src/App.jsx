378030363c60561f990d9b7f5f15d5461eada9ca
Ankit Raj <ruby08241@gmail.com>
2023-06-11 13:25:02 +0530

    used ContextAPI for Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,103 @@
-import { useEffect, useState } from 'react';
-import './app.css'
+import { useEffect, useState, createContext, useContext } from 'react';
+import './app.css';
+const GameContext = createContext();
 export default function App() {
   const [isWinner, setWinner] = useState(false);
   const [turn, setTurn] = useState('X');
   const [data_X, setData_X] = useState([]);
   const [data_Y, setData_Y] = useState([]);
-  const [pos, setPos] = useState(0);
-  const[reset, setReset] = useState(false);
+  const [reset, setReset] = useState(false);
+
   useEffect(() => {
     if (checkWinner(data_X)) {
       setTurn("X has Won");
     }
-    else if(checkWinner(data_Y)){
+    else if (checkWinner(data_Y)) {
       setTurn("O has Won");
     }
-    else if(checkTie(data_X, data_Y)){
+    else if (checkTie(data_X, data_Y)) {
       setTurn("Tie");
     }
-    
+
   })
-  
+
   return (
+    <GameContext.Provider  
+    value = {{
+      turn,
+      setTurn,
+      data_X,
+      setData_X,
+      data_Y,
+      setData_Y,
+      isWinner,
+      setWinner,
+      reset,
+      setReset
+    }}>
       <div className='boardWrapper'>
         <div className='board'>
           <div className="board-row">
-            <Square  turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={1} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
-            <Square  turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={2} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
-            <Square  turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={3} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
+            <Square pos={1} />
+            <Square pos={2} />
+            <Square pos={3} />
           </div>
           <div className="board-row">
-            <Square turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={4} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
-            <Square turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={5} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
-            <Square turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={6} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
+            <Square pos={4} />
+            <Square pos={5} />
+            <Square pos={6} />
           </div>
           <div className="board-row">
-            <Square turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={7} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
-            <Square turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={8} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
-            <Square turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={9} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
+            <Square pos={7} />
+            <Square pos={8} />
+            <Square pos={9} />
           </div>
         </div>
         <div className='turnIndicator'>{turn}</div>
-        <ResetButton turn={turn} setTurn={setTurn} data_X={data_X} setData_X={setData_X} data_Y={data_Y} setData_Y={setData_Y} pos={9} isWinner={isWinner} setWinner={setWinner} reset = {reset} setReset = {setReset}/>
-        <div className='aboutWrapper'><div className = "about">Made By Ankit Raj</div></div>
+        <ResetButton />
+        <div className='aboutWrapper'><div className="about">Made By Ankit Raj</div></div>
       </div>
-    
+    </GameContext.Provider>
   )
 }
-function Square({ turn, setTurn, data_X, setData_X, data_Y, setData_Y, isWinner, setWinner, pos, reset, setReset}){
+function Square({pos}) {
+  const { turn, setTurn, data_X, setData_X, data_Y, setData_Y, isWinner, setWinner, reset, setReset } = useContext(
+    GameContext
+  );
   const [value, setValue] = useState(null);
-  useEffect(()=>{
-    if(reset){
+  useEffect(() => {
+    if (reset) {
       setValue(null);
     }
-  },[reset])
+  }, [reset])
   function handleClick() {
-      setReset(false);
-      if (!(data_Y.includes(pos) || data_X.includes(pos))) {
-        if (turn === 'X') {
-          setValue('X');
-          setTurn('O');
-          const newData = [...data_X];
-          newData.push(pos);
-          setData_X(newData);
-          if (checkWinner(data_X)) {
-            setWinner(true);
-          }
-          else {
-            setWinner(false);
-          }
-        } else if (turn === "O") {
-          setValue('O');
-          setTurn('X');
-          const newData = [...data_Y];
-          newData.push(pos);
-          setData_Y(newData);
+    setReset(false);
+    if (!(data_Y.includes(pos) || data_X.includes(pos))) {
+      if (turn === 'X') {
+        setValue('X');
+        setTurn('O');
+        const newData = [...data_X];
+        newData.push(pos);
+        setData_X(newData);
+        if (checkWinner(data_X)) {
+          setWinner(true);
         }
+        else {
+          setWinner(false);
+        }
+      } else if (turn === "O") {
+        setValue('O');
+        setTurn('X');
+        const newData = [...data_Y];
+        newData.push(pos);
+        setData_Y(newData);
       }
-      else {
-        console.log("Square Occupied");
-      }
-      
-    
+    }
+    else {
+      console.log("Square Occupied");
+    }
+
+
   }
   return (
     <>
@@ -90,7 +107,8 @@ function Square({ turn, setTurn, data_X, setData_X, data_Y, setData_Y, isWinner,
     </>
   );
 }
-function ResetButton({ turn, setTurn, data_X, setData_X, data_Y, setData_Y, isWinner, setWinner, reset, setReset }) {
+function ResetButton() {
+  const {turn, setTurn, data_X, setData_X, data_Y, setData_Y, isWinner, setWinner, reset, setReset} = useContext(GameContext);
   function handleClick() {
     setData_X([]);
     setData_Y([]);
@@ -121,7 +139,7 @@ function checkWinner(playerArray) {
   );
 }
 function checkTie(data_X, data_Y) {
-  if (data_X.length + data_Y.length  == 9) {
+  if (data_X.length + data_Y.length == 9) {
     return true;
   }
   return false;
